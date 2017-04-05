aade0b19ce83f25c97e2a7ece25ad99fb03f26dd
Pablo Peillard <pablo12335@gmail.com>
2017-04-05 15:01:18 -0500

    patient isn't a nurse table

--- init.js
+++ init.js
@@ -24,6 +24,11 @@ const query = client.query(
 
 // Patient
 const query = client.query(
+  'CREATE TABLE Patient(SSN PRIMARY KEY CHAR(9) NOT NULL, Next_of_Kin VARCHAR(80), LivingWill VARCHAR(80), Name VARCHAR(80), Phone CHAR(10), Address VARCHAR(120), R_Timeslot DATE, O_Timeslot DATE, Insurance varchar(80) references Insurance(Name), R_Room varchar(80) references Room(Room No), O_Room varchar(80) references Room(Room No), Tech_SSN char(9) references Technician(SSN), Nurse_SSN char(9) references Nurse(SSN), Surgeon_SSN char(9) references Surgeon(SSN), C_Name VARCHAR(80) references Condition(Name))'
+);
+
+// Patient
+const query = client.query(
   'CREATE TABLE Nurse(SSN PRIMARY KEY CHAR(9) NOT NULL, Next_of_Kin VARCHAR(80), LivingWill VARCHAR(80), Name VARCHAR(80), Phone CHAR(10), Address VARCHAR(120), R_Timeslot DATE, O_Timeslot DATE, Insurance varchar(80) references Insurance(Name), R_Room varchar(80) references Room(Room No), O_Room varchar(80) references Room(Room No), Tech_SSN char(9) references Technician(SSN), Nurse_SSN char(9) references Nurse(SSN), Surgeon_SSN char(9) references Surgeon(SSN), C_Name VARCHAR(80) references Condition(Name))'
 );
 
